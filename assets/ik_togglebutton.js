9056a726c7457de1f58abb8b5c21528be7e5f6fc
Kate McTigue <kathryn.stoffregen@gmail.com>
2018-10-02 15:35:13 -0500

    unit 4 - toggle button

--- assets/ik_togglebutton.js
+++ assets/ik_togglebutton.js
@@ -31,18 +31,18 @@
 		id = 'toggle' + $('.ik_togglebutton').length; // generate unique id
 		$elem = this.element
 			.attr({
-				"id": id //,
-//			"tabindex": 0,
-//			"role": "button",
-//			"aria-label": plugin.options.label,
-//			"aria-pressed": false
+				"id": id,
+				"tabindex": 0,
+				"role": "button",
+				"aria-label": plugin.options.label,
+				"aria-pressed": false
 			});
 		
 		plugin.options.onToggle = plugin.options.onToggle.bind(plugin);
 		
 		$elem
 			.on('click', {plugin: plugin}, plugin.onActivate)
-//		.on('keydown', {plugin: plugin}, plugin.onActivate)
+			// .on('keydown', {plugin: plugin}, plugin.onActivate)
 		;
 		
 	};
