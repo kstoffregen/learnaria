8b6acbd51bdb8b13cf757c3c0a059c9c55d8fc24
Kate McTigue <kathryn.stoffregen@gmail.com>
2018-10-16 11:06:47 -0500

    activity 6 - tooltip

--- assets/ik_tooltip.js
+++ assets/ik_tooltip.js
@@ -33,19 +33,38 @@
 		
 		if(tip.length > 0) {
 			
+			/*
+				Define the tooltip with role="tooltip". Hide the tooltip by default with aria-hidden="true". 
+				Also add a live region with aria-live="polite" so screen readers automatically read the tooltip 
+				when it appears. Note that the WAI-ARIA 1.1 best practices recommend using aria-describedby 
+				within the owning element to reference the content of a tooltip, which does not announce as 
+				expected with current versions of Chrome, so here we have decided to use aria-live, which 
+				announces correctly across all current browsers.
+			*/
 			$tooltip = $('<span/>') // create tooltip
 				.text(tip)
 				.addClass('ik_tooltip')
 				.attr({
 					'id': id,
+					'role': 'tooltip', // assign tooltip role
+			        'aria-hidden': 'true', // hide it from screen reader to prevent it from been read twice
+			        'aria-live': 'polite' // make it live region
 				});
 			
+			/*
+				Add keyboard focus to the element the tooltip belongs to with tabindex="0", and 
+				add focus to .on('mouseover') so both a mouse hover and keyboard focus open the tooltip.
+			*/
 			$elem
+				.attr({
+			        'tabindex': 0 // add tab order
+			    })
 				.css('position', 'relative')
 				.removeAttr('title') // remove title to prevent it from being read
 				.after($tooltip)
-				.on('mouseover', function(event) {
-					
+				//.on('mouseover', function(event) {
+				.on('mouseover focus', function(event) {
+
 					var y, x;
 					
 					y = $elem.position().top - $tooltip.height() - 20;
@@ -61,22 +80,61 @@
 						$tooltip.addClass('mouseover'); // add mouseover class when mouse moves over the current element
 					}
 					
+					/*
+						Add aria-hidden="false" so the hidden-by-default tooltip becomes visible when the mouse hover or keyboard focus occurs.
+					*/
 					$tooltip // position and show tooltip
+						.attr({
+				        	'aria-hidden': 'false'
+				        })
 						.css({
 							'top': y, 
 							'left': x
 						})
 						.addClass('visible');
 				})
+				/*
+					Add aria-hidden="true" to be sure the tooltip is hidden from screen readers, should a mouseout event close the tooltip, 
+					adding it to .on(mouseout) chained to the element ($elem) definition.
+				*/
 				.on('mouseout', function(event) {
 					
 					if (!$(event.currentTarget).is(':focus') ) { // hide tooltip if current element is not focused
 						
 						$tooltip
+							.attr({
+				            	'aria-hidden': 'true'
+				            })
 							.removeClass('visible mouseover');					
 					}
 										
 				})
+				/*
+					As a keyboard equivalent for the .on(mouseout) described above, .on(blur) is chained to the $elem element and 
+					within it aria-hidden="true" hides the tooltip again, if the mouse pointer is not over the element.
+				*/
+				.on('blur', function(event) {              
+				    if (!$tooltip.hasClass('mouseover') ) { // hide tooltip if mouse is not over the current element               
+				    $tooltip
+				        .attr({
+				            'aria-hidden': 'true'
+				        })
+				        .removeClass('visible');       
+				    }
+				})
+				/*
+					If the Esc key is used, add aria-hidden="true" to hide the tooltip, even if the mouse is hovering, or the owning element has focus.
+				*/
+				.on('keyup', function(event) {         
+				    if(event.keyCode == ik_utils.keys.esc) { // hide when escape key is pressed
+				        $tooltip
+				            .attr({
+				                'aria-hidden': 'true'
+				            })
+				            .removeClass('visible');
+				    }              
+				});
+
 		}
 	};
 	
