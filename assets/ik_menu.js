a641611b8311e5af26b4b948a33729d360d92032
Kate McTigue <kathryn.stoffregen@gmail.com>
2018-10-18 16:22:57 -0500

    activity 12 - menu bar

--- assets/ik_menu.js
+++ assets/ik_menu.js
@@ -1,7 +1,13 @@
 ;(function ( $, window, document, undefined ) {
  
+ 	/*
+ 		Note:
+ 		Provide some instructions on how to use the menu with a keyboard and add them to the default options.
+	*/
 	var pluginName = 'ik_menu',
-		defaults = {};
+		defaults = {
+        	'instructions': 'Use arrow keys to navigate between menuitems, spacebar to expand submenus, escape key to close submenus, enter to activate menuitems.'
+		};
 	 
 	/**
 	 * @constructs Plugin
@@ -13,7 +19,7 @@
 		this._name = pluginName;
 		this._defaults = defaults;
 		this.element = $(element);
-		this.options = $.extend( {}, defaults, options) ;
+		this.options = $.extend( {}, defaults, options);
 		
 		this.init();
 	}
@@ -36,13 +42,25 @@
 			.text(plugin.options.instructions) // get instruction text from plugin options
 			.addClass('ik_readersonly') // hide element from visual display
 			.attr({
-				'id': id + '_instructions'
+				'id': id + '_instructions',
+				'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
 			})
 			.appendTo(this.element);
 			
+		/* MENU BAR */	
 		$elem.find('ul:eq(0)')
 			.attr({
-				'id': id
+				'id': id,
+		        'role': 'menubar', // assign menubar role to the topmost ul element
+		        'tabindex': 0,
+		        'aria-labelledby': id + '_instructions'
+			});
+
+		/* MENU BUTTON */
+		$elem.find('li>ul')
+			.attr({
+			    'role': 'menu',
+			    'aria-hidden': true // hide submenus from screen reader
 			});
 		
 		plugin.menuitems = $elem.find('li') // setup menuitems
@@ -52,30 +70,57 @@
 				var $me, $link;
 				
 				$me = $(this);
-				$link = $me.find('>a');
+				$link = $me.find('>a')
+					.attr({ // disable links
+					    'tabindex': -1,
+					    'aria-hidden': true
+					});
+
+				$me.attr({
+				    'role': 'menuitem', // assign menuitem rols
+				    'tabindex': -1,  // remove from tab order
+				    'aria-label': $link.text() // label with link text
+				});
 				
-				$me.has('ul').addClass('expandable');
+				/*
+					Note:
+					For each of the menu items that has a submenu.
+				*/
+				$me.has('ul')
+				    .attr({ // setup submenus
+					    'aria-haspopup': true, // announce the presence of the submenu
+					    'aria-expanded': false // default state to “collapsed”
+				    })
+					.addClass('expandable');
 			});
 		
 		plugin.selected = plugin.menuitems // setup selected menuitem
-			.find('.selected');
+			.find('.selected')
+			.attr({
+		        'tabindex': 0,
+		        'aria-selected': true
+		    });
 		
+		/* Add keyboard access back to menu items */
 		if (!plugin.selected.length) {
-			
 			plugin.menuitems
-				.eq(0);
-			
+				.eq(0)
+				.attr({
+		            'tabindex': 0 
+		        });
 		} else {
-			
 			plugin.selected
-				.parentsUntil('nav', 'li');
-			
+				.parentsUntil('nav', 'li')
+				.attr({
+		        	'tabindex': 0
+		        });
 		}
 		
 		plugin.menuitems // setup event handlers
 			.on('mouseenter', plugin.showSubmenu)
 			.on('mouseleave', plugin.hideSubmenu)
-			.on('click', {'plugin': plugin}, plugin.activateMenuItem);
+			.on('click', {'plugin': plugin}, plugin.activateMenuItem)
+			.on("keydown", {'plugin': plugin}, plugin.onKeyDown); // reference the keyboard access class
 			
 		$(window).on('resize', function(){ plugin.collapseAll(plugin); } ); // collapse all submenues when window is resized
 		
@@ -94,7 +139,16 @@
 		$submenu = $elem.children('ul');
 		
 		if ($submenu.length) {
-			$elem.addClass('expanded');
+			$elem
+				.addClass('expanded')
+				.attr({
+		            'aria-expanded': true,
+		            'tabindex': -1
+		        });
+	        $submenu
+		        .attr({
+		            'aria-hidden': false
+		        });
 		}
 	};
 	
@@ -111,8 +165,11 @@
 		$submenu = $elem.children('ul');
 		
 		if ($submenu.length) {
-			$elem.removeClass('expanded');
-
+			$elem
+				.removeClass('expanded')
+				.attr({'aria-expanded': false});
+			$submenu.attr({'aria-hidden': true});
+    		$submenu.children('li').attr({'tabindex': -1});
 		}
 	}
 	
@@ -125,7 +182,14 @@
 	Plugin.prototype.collapseAll = function(plugin, $elem) {
 		
 		$elem = $elem || plugin.element;
-		
+
+		/*
+			Note:
+			Collapse any open menus by reversing all attributes defining the element as open.
+		*/
+	    $elem.find('[aria-hidden=false]').attr({'aria-hidden': true});
+	    $elem.find('.expanded').removeClass('expanded').attr({'aria-expanded': false});
+	    $elem.find('li').attr({'tabindex': -1}).eq(0).attr({'tabindex': 0});
 	};
 	
 	/** 
@@ -151,7 +215,88 @@
 		}
 		
 	};
-	
+
+	/**
+	 * Selects specified tab.
+	 *
+	 * @param {Object} event - Keyboard event.
+	 * @param {object} event.data - Event data.
+	 * @param {object} event.data.plugin - Reference to plugin.
+	 * @param {object} event.data.index - Index of a tab to be selected.
+	 */
+	 
+	 
+	Plugin.prototype.onKeyDown = function (event) {
+	   
+	    var plugin, $elem, $current, $next, $parent, $submenu, $selected;
+	   
+	    plugin = event.data.plugin;
+	    $elem = $(plugin.element);
+	    $current = $(plugin.element).find(':focus');
+	    $submenu = $current.children('ul');
+	    $parentmenu = $($current.parent('ul'));
+	    $parentitem = $parentmenu.parent('li');
+	       
+	    switch (event.keyCode) {
+	        case ik_utils.keys.right:
+	            event.preventDefault();
+	            if ($current.parents('ul').length == 1) {
+	                $current.attr({'tabindex': -1}).next('li').attr({'tabindex': 0}).focus();
+	            }
+	            break;
+	       
+	        case ik_utils.keys.left:
+	            event.preventDefault();
+	            if ($current.parents('ul').length == 1) {
+	                $current.attr({'tabindex': -1}).prev('li').attr({'tabindex': 0}).focus();
+	            }
+	            break;
+	           
+	        case ik_utils.keys.up:
+	            event.preventDefault();
+	            event.stopPropagation();
+	            if ($current.parents('ul').length > 1) {
+	                $current.attr({'tabindex': -1}).prev('li').attr({'tabindex': 0}).focus();
+	            }
+	            break;
+	       
+	        case ik_utils.keys.down:
+	            event.preventDefault();
+	            event.stopPropagation();
+	            if($current.parents('ul').length > 1) {
+	                $current.attr({'tabindex': -1}).next('li').attr({'tabindex': 0}).focus();
+	            }
+	            break;
+	           
+	        case ik_utils.keys.space:
+	            event.preventDefault();
+	            event.stopPropagation();
+	            if($submenu.length) {
+	                plugin.showSubmenu(event);
+	                $submenu.children('li:eq(0)').attr({'tabindex': 0}).focus();
+	            }
+	            break;
+	       
+	        case ik_utils.keys.esc:
+	            event.stopPropagation();
+	            if ($parentitem.hasClass('expandable')) {
+	                $parentitem.removeClass('expanded').attr({
+	                    'tabindex': 0,
+	                    'aria-expanded': false
+	                }).focus();
+	                plugin.collapseAll(plugin, $parentitem);
+	            }
+	            break;
+	       
+	        case ik_utils.keys.enter:
+	            plugin.activateMenuItem(event);
+	            break;
+
+	        case ik_utils.keys.tab:
+	            plugin.collapseAll(plugin);
+	            break;
+	    }
+	}
 	
 	$.fn[pluginName] = function ( options ) {
 		
