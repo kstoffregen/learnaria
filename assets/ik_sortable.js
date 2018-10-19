79deb447eb4fcb49e339100bfcc9b93ceb090abb
Kate McTigue <kathryn.stoffregen@gmail.com>
2018-10-19 16:09:48 -0500

    activity 14 - sortable list

--- assets/ik_sortable.js
+++ assets/ik_sortable.js
@@ -1,7 +1,17 @@
 ;(function ( $, window, document, undefined ) {
- 
+
+/*
+	Note:
+	Create instructions on using the sortable list with a keyboard. We also want to determine which 
+	modifier key to include in the instructions. For Mac it will be the Command key, otherwise it 
+	will be the Ctrl key. Here the standard accesskey key commands will also work as the modifier 
+	and can potentially be described as well (e.g. Ctrl+Alt on Mac, or Ctrl on Windows).
+*/
 var pluginName = "ik_sortable",
-	defaults = {};
+	modifier = navigator.platform.indexOf('Mac') > -1 ? 'Command' : 'Control',
+	defaults = {
+		'instructions': 'Use arrow keys to select a list item,  ' + modifier + ' + arrow keys to move it to a new position.'
+	};
 	 
 	function Plugin( element, options ) {
 		
@@ -19,28 +29,41 @@ var pluginName = "ik_sortable",
 		
 		plugin = this;
 		id = 'sortable_' + $('.ik_sortable').length;
-		$elem = this.element.attr({
-			'id': id
-		})
-		.wrap('<div class="ik_sortable"></div>').before(plugin.temp);
+		$elem = this.element
+			.attr({
+				'id': id,
+			    'role': 'list',
+			    'tabindex': 0,
+			    'aria-labelledby': id + '_instructions'
+			})
+			.wrap('<div class="ik_sortable"></div>').before(plugin.temp);
 			
 		total = $elem.children('li').length;
 			
-		plugin.items = $elem.children('li').each( function(i, el) {
+		plugin.items = $elem.children('li')
+			.each( function(i, el) {
 			
-			$(el).attr({
-				'draggable': true,
-				'id': id + '_' + i
-			});
-		})
-		.on('dragstart', {'plugin': plugin}, plugin.onDragStart)
-		.on('drop', {'plugin': plugin}, plugin.onDrop)
-		.on('dragend', {'plugin': plugin}, plugin.onDragEnd)
-		.on('dragenter', {'plugin': plugin}, plugin.onDragEnter)
-		.on('dragover', {'plugin': plugin}, plugin.onDragOver)
-		.on('dragleave', {'plugin': plugin}, plugin.onDragLeave);
+				$(el).attr({
+					'draggable': true,
+					'id': id + '_' + i
+				});
+			})
+			.on('dragstart', {'plugin': plugin}, plugin.onDragStart)
+			.on('drop', {'plugin': plugin}, plugin.onDrop)
+			.on('dragend', {'plugin': plugin}, plugin.onDragEnd)
+			.on('dragenter', {'plugin': plugin}, plugin.onDragEnter)
+			.on('dragover', {'plugin': plugin}, plugin.onDragOver)
+			.on('dragleave', {'plugin': plugin}, plugin.onDragLeave)
+			.on('keydown', {'plugin': plugin}, plugin.onKeyDown);
 		
-		
+		$('<div/>') // add div element to be used with aria-describedby attribute of the menu
+		    .text(plugin.options.instructions) // get instruction text from plugin options
+		    .addClass('ik_readersonly') // hide element from visual display
+		    .attr({
+			    'id': id + '_instructions',
+			    'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
+			})
+		    .appendTo($elem);		
 	};
 	
 	// dragged item
@@ -127,9 +150,50 @@ var pluginName = "ik_sortable",
 		
 		plugin.items.each( function(i, el) {
 			var $me = $(el);
-		});
-		
+
+			// Update the label for moved items to reflect their new position in the list
+			$me.attr({
+		        'aria-label': $me.text() + ' ' + (i + 1) + ' of ' + plugin.items.length + ' movable' 
+		    });
+		});	
 	}
+
+	Plugin.prototype.onKeyDown = function (event) {
+ 
+        var plugin, $me, currIndex, nextIndex;
+       
+        plugin = event.data.plugin;
+        $me = $(event.currentTarget);
+        currentIndex = plugin.items.index(event.currentTarget);
+           
+        switch (event.keyCode) {
+            case ik_utils.keys.down:
+                plugin.items.attr({'tabindex': -1});
+                if(currentIndex < plugin.items.length - 1) {
+                    if(event.ctrlKey || event.metaKey) { // move item down
+                        $me.insertAfter( $me.next() );
+                        $me.attr({'tabindex': 0}).focus();
+                        plugin.resetNumbering(plugin);
+                    } else { // move focus to the next item
+                        $me.next().attr({'tabindex': 0}).focus();
+                    }
+                }
+                break;
+           
+            case ik_utils.keys.up:
+                plugin.items.attr({'tabindex': -1});
+                if(currentIndex > 0) {
+                    if(event.ctrlKey || event.metaKey) { // move item up
+                        $me.insertBefore( $me.prev() );
+                        $me.attr({'tabindex': 0}).focus();
+                        plugin.resetNumbering(plugin);
+                    } else { // move focus to the previous item
+                        $me.prev().attr({'tabindex': 0}).focus();
+                    }
+                }
+                break;
+        }
+	};
 
 	$.fn[pluginName] = function ( options ) {
 		
